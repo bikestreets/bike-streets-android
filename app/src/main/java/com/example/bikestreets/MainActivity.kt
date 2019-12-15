e36a200f764b870d6810d7f0b2edaaa25a1a1dda
Jeremy Zornow <jeremy@zornow.com>
2019-12-14 21:10:02 -0700

    Initial Commit: ArcGIS Map Application Tutorial
    This was produced by following the instructions described here:
    https://developers.arcgis.com/android/latest/guide/develop-your-first-map-app-with-kotlin.htm
    
    This will serve as the basis for our application. Layers and
    geolocation pending.

---
+++ app/src/main/java/com/example/bikestreets/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.bikestreets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import kotlinx.android.synthetic.main.activity_main.mapView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // create a map with the BasemapType topographic
+        val map = ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16)
+
+        // set the map to be displayed in the layout's MapView
+        mapView.map = map
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.pause()
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.dispose()
+    }
+}
