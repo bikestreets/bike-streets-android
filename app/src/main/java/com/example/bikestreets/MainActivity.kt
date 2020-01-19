2e96b45c876dc684f6d158e377e461727741630a
Jeremy Zornow <jeremy@zornow.com>
2020-01-19 13:53:20 -0700

    Initial attempt to convert to MapBox
    MapBox offers higher-order tooling to handle permission requests.
    Figuring out how to use those tools (specifically PermissionsManager)
    proved to be a little bit difficult.
    
    At this point, we're successfully requesting and setting permissions.
    Next time around, we'll figure out what to *do* with those permissions
    once they've been requested.

--- app/src/main/java/com/example/bikestreets/MainActivity.kt
+++ app/src/main/java/com/example/bikestreets/MainActivity.kt
@@ -1,140 +1,94 @@
 package com.example.bikestreets
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.esri.arcgisruntime.mapping.ArcGISMap
-import com.esri.arcgisruntime.mapping.Basemap
-import kotlinx.android.synthetic.main.activity_main.mapView
-import com.esri.arcgisruntime.mapping.view.MapView
-import com.esri.arcgisruntime.mapping.view.LocationDisplay
-import android.Manifest;
-import android.content.pm.PackageManager
-import android.util.Log
-import androidx.core.content.ContextCompat
-import 	androidx.core.app.ActivityCompat
-import android.widget.Toast
-import com.esri.arcgisruntime.layers.KmlLayer
-import com.esri.arcgisruntime.loadable.LoadStatus
-import com.esri.arcgisruntime.ogc.kml.KmlDataset
+import android.os.Bundle;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.android.core.permissions.PermissionsListener
 
 class MainActivity : AppCompatActivity() {
-    var mLocationDisplay: LocationDisplay ?= null
-    var mArcGISMap: ArcGISMap ?= null
-
-    val REPO_URL = "https://raw.githubusercontent.com/bikestreets/denver-map/master"
-    val LAYERS_TO_LOAD = listOf(
-        "1-bikestreets-master-v0.3.kml",
-        "2-trails-master-v0.3.kml",
-        "3-bikelanes-master-v0.3.kml",
-        "4-bikesidewalks-master-v0.3.kml",
-        "5-walk-master-v0.3.kml"
-    )
-
-    fun dataSourceStatusChangedHandler(dataSourceStatusChangedEvent: LocationDisplay.DataSourceStatusChangedEvent) {
-        if (dataSourceStatusChangedEvent.isStarted() || dataSourceStatusChangedEvent.getError() == null) return
-
-        val requestPermissionsCode: Int = 2;
-        val requestPermissions: Array<String> = arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        );
-
-//        if (!(ContextCompat.checkSelfPermission(MainActivity.this, requestPermissions[0]) == PackageManager.PERMISSION_GRANTED
-        if(ContextCompat.checkSelfPermission(this, requestPermissions[0]) != PackageManager.PERMISSION_GRANTED
-            && ContextCompat.checkSelfPermission(this, requestPermissions[1]) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, requestPermissions, requestPermissionsCode);
-        } else {
-            var message: String = String.format(
-                "Error in DataSourceStatusChangedListener: %s",
-                dataSourceStatusChangedEvent.getSource().getLocationDataSource().getError().message
-            );
-
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-        }
-
-        mLocationDisplay?.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
-        mLocationDisplay?.startAsync();
-    }
-
-    fun setupLocationDisplay() {
-        mLocationDisplay = mapView?.getLocationDisplay();
-
-        mLocationDisplay?.addDataSourceStatusChangedListener(::dataSourceStatusChangedHandler);
-
-        // activate auto-pan mode
-        mLocationDisplay?.autoPanMode = LocationDisplay.AutoPanMode.RECENTER
-
-        // start locationDisplay services
-        mLocationDisplay?.startAsync()
-    }
-
-    fun addKmlLayers() {
-        for(layerName in LAYERS_TO_LOAD) {
-            // loop through all URIs to load
-            var dataset = KmlDataset("$REPO_URL/$layerName")
-
-            // add listener to report load errors
-            dataset.addDoneLoadingListener(fun () {
-                if (dataset.loadStatus != LoadStatus.LOADED) {
-                    var error = "Failed to load kml layer from URL: " + dataset.loadError.message
-                    Toast.makeText(this, error, Toast.LENGTH_LONG).show()
-                    Log.e("addKmlLayers", error)
-                } else {
-                    // press into a KML Layer
-                    var kmlLayer = KmlLayer(dataset)
-
-                    // add layer to the map's set of operational layers
-                    mArcGISMap?.operationalLayers?.add(kmlLayer)
-                }
-            })
-
-            // load from data source
-            dataset.loadAsync()
-        }
-    }
-
-    fun setupArcGISMap() {
-        mArcGISMap = ArcGISMap(Basemap.Type.TOPOGRAPHIC, 87.740054, -104.946276, 16)
-
-        addKmlLayers()
-
-        mapView.map = mArcGISMap
-    }
+    private var mapView: MapView? = null
+    private var permissionsManager: PermissionsManager ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, "pk.eyJ1Ijoianpvcm5vdyIsImEiOiJjazVsOWhkc2YwbWgwM2xuNXJvdnlhN2o3In0.tW5TbWlDY-ciFrYSv6qTOA")
+
         setContentView(R.layout.activity_main)
 
-        // create a map with the BasemapType topographic
-        setupArcGISMap()
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+            }
+        }
 
-        // setup Location Display
-        setupLocationDisplay()
+        if (PermissionsManager.areLocationPermissionsGranted(this)){
+            // Permission sensitive logic called here, such as activating the Maps SDK's LocationComponent to show the device's location
+        } else {
+            var permissionsListener: PermissionsListener = object : PermissionsListener {
+                override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+                    // provides explanation of why permission is required
+                }
+
+                override fun onPermissionResult(granted: Boolean) {
+                    if (granted) {
+                        // Permission sensitive logic called here, such as activating the Maps SDK's LocationComponent to show the device's location
+
+
+                    } else {
+                        // User denied the permission
+                    }
+                }
+            }
+
+            permissionsManager = PermissionsManager(permissionsListener)
+            permissionsManager?.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        mapView.pause()
-    }
-    override fun onResume() {
-        super.onResume()
-        mapView.resume()
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.dispose()
+        mapView?.onPause()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            mLocationDisplay?.startAsync();
-        } else {
-            Toast.makeText(this, resources.getString(R.string.location_permission_denied), Toast.LENGTH_SHORT)
-        }
-        // super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
     }
 }
+
