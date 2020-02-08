ee11e1fc9a026df7fd20b7b6c88797f635766574
Jeremy Zornow <jeremy@zornow.com>
2020-02-08 13:06:18 -0700

    Add Bikestreets map layers

--- app/src/main/java/com/example/bikestreets/MainActivity.kt
+++ app/src/main/java/com/example/bikestreets/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.bikestreets
 
-import android.os.Bundle
+// used to handle geojson loading
+import java.io.InputStream
+import android.content.res.AssetManager
+
+// used to handle geojson map layer drawing
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.mapboxsdk.style.layers.LineLayer
+import com.mapbox.mapboxsdk.style.layers.Property
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory
 import android.graphics.Color
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
 import com.mapbox.mapboxsdk.location.modes.CameraMode
 import com.mapbox.mapboxsdk.location.modes.RenderMode
-
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import java.util.*
 
-
-import com.mapbox.mapboxsdk.location.LocationComponentOptions
-import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
-
-import androidx.appcompat.app.AppCompatActivity;
-import com.mapbox.android.core.permissions.PermissionsManager
-import com.mapbox.android.core.permissions.PermissionsListener
 
 class MainActivity : AppCompatActivity() {
     private var mapView: MapView? = null
@@ -35,6 +44,9 @@ class MainActivity : AppCompatActivity() {
             mapboxMap.setStyle(Style.MAPBOX_STREETS) {
                 // Map is set up and the style has loaded. Now you can add data or make other map adjustments
                 showDeviceLocation(mapboxMap, it)
+
+                // add geojson layers
+                showMapLayers(this, it)
             }
         }
 
@@ -60,6 +72,49 @@ class MainActivity : AppCompatActivity() {
             permissionsManager = PermissionsManager(permissionsListener)
             permissionsManager?.requestLocationPermissions(this)
         }
+    }
+
+    fun showMapLayers(activity: MainActivity, mapStyle: Style) {
+        var mAssetManager: AssetManager = activity.getAssets()
+        val root: String = "geojson"
+
+        mAssetManager.list("$root/").forEach { fileName ->
+            var featureCollection = featureCollectionFromStream(
+                mAssetManager.open("$root/$fileName")
+            )
+
+            createLineLayer(fileName, featureCollection, mapStyle)
+        }
+
+    }
+
+    fun featureCollectionFromStream(fileStream: InputStream): FeatureCollection {
+        var geoJsonString = convertStreamToString(fileStream)
+
+        return FeatureCollection.fromJson(geoJsonString)
+    }
+
+    fun createLineLayer(layerName: String, featureCollection: FeatureCollection, mapStyle: Style) {
+        if(featureCollection.features() != null) {
+            mapStyle.addSource(GeoJsonSource(layerName, featureCollection))
+
+            // The layer properties for our line. This is where we make the line dotted, set the
+            // color, etc.
+            var lineLayer = LineLayer("$layerName-id", layerName)
+                .withProperties(
+                    PropertyFactory.lineCap(Property.LINE_CAP_SQUARE),
+                    PropertyFactory.lineJoin(Property.LINE_JOIN_MITER),
+                    PropertyFactory.lineOpacity(.7f),
+                    PropertyFactory.lineWidth(7f),
+                    PropertyFactory.lineColor(Color.parseColor("#3bb2d0")))
+
+            mapStyle.addLayer(lineLayer)
+        }
+    }
+
+    fun convertStreamToString(input: InputStream): String {
+        val scanner = Scanner(input).useDelimiter("\\A")
+        return if (scanner.hasNext()) scanner.next() else ""
     }
 
     fun showDeviceLocation(mapboxMap: MapboxMap, style: Style) {
