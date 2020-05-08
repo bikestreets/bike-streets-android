170b1e5a279043ddd7af5565a373a6ae56f86f18
Jeremy Zornow <jeremy@zornow.com>
2020-05-07 21:26:45 -0600

    Create settings section on About Page
    Please note: this is really just the view and persistence logic for
    the settings form. These settings are saved when a user changes them
    but they *are not read by the application yet* so they don't actually
    affect any behavior. That will come in later commits: sit tight.

--- app/src/main/java/com/application/bikestreets/About.kt
+++ app/src/main/java/com/application/bikestreets/About.kt
@@ -1,12 +1,30 @@
 package com.application.bikestreets
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
 
 class About : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        // This layout is designed only for portriat mode, so force the user to that orientation
+        // regardless of device settings
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
     }
 }
