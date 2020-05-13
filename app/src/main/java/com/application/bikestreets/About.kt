c0c46a3dfb95f2c7cb15ec671cb23f75d28b59a6
Jeremy Zornow <jeremy@zornow.com>
2020-05-12 19:43:47 -0600

    Add "Send Feedback" link
    This creates an intent that will either (a) open externally in chrome
    if the user has no other browsers installed or (b) will prompt the user
    to select an appropriate application to open the link in.

--- app/src/main/java/com/application/bikestreets/About.kt
+++ app/src/main/java/com/application/bikestreets/About.kt
@@ -1,11 +1,15 @@
 package com.application.bikestreets
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.preference.PreferenceFragmentCompat
 
 class About : AppCompatActivity() {
+    private val feedbackUrl = Uri.parse("https://www.bikestreets.com/mobile-app-feedback")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +24,20 @@ class About : AppCompatActivity() {
             .replace(R.id.settings, SettingsFragment())
             .commit()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        activateFeedbackButton()
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
+    }
+
+    fun activateFeedbackButton() {
+       findViewById<Button>(R.id.feedback_button).setOnClickListener {
+           val intent = Intent(Intent.ACTION_VIEW).setData(feedbackUrl)
+           startActivity(intent)
+       }
     }
 }
